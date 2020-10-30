567fe4c4710d1e7544160a43fc26498ced89f9af
isapoff <iskender.saparov.is@gmail.com>
2020-10-30 20:05:05 +0600

    First commit

---
+++ task1.py
@@ -0,0 +1,25 @@
+#1.
+# number = (8 + 12) * 2
+# print (number)
+
+#2.
+# a = 15
+# b = 4
+# print(15 % 4)
+
+# #3.
+# num1 = 16
+# print(num1)
+# number = input("You are age:")
+# print(number)
+
+# #4.
+# a = 25 
+# b = 5
+# print(a + b) 
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+
+
---
+++ task2.py
@@ -0,0 +1,44 @@
+# 1
+# a = "Hello "
+# b = "world, "
+# c = "I'm "
+# d = "Isken! "
+# print(input(a + "" + b + "" + c + "" + d))
+
+# 2.
+# name = 'Isken '
+# result = 3  *  name
+# print(result)
+
+# # 3.
+# name = "John "
+# surname = "Snow "
+# full_name = name + "" + surname
+# name = input("Hello, " + "" + full_name)
+
+# 4.
+# word = 'safari'
+# print(word[::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
